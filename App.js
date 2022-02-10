7a48017c80bf04f0086bc01df499ab8b34872964
Sapana Sunil Gonarkar <63834988+sapana10@users.noreply.github.com>
2022-02-10 15:51:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import appStyles from "./style.css";
+
+function App() {
+
+  const [userData, setUserData] = useState([]);
+   
+  const fetchData = async function() {
+    const url = "https://reqres.in/api/users?page=1";
+    const response = await fetch(url);
+    const data = await response.json();
+    setUserData(data.data);
+};
+
+return (
+  <div>
+    <div className="navBar">
+      <h2 className="navBrand">Task 2</h2>
+      <button onClick ={fetchData} className="navButton">Get Users</button>
+    </div>
+    <h1 id="containerHeading">User Details</h1>
+    <div className="container">
+      {userData.map(function(user){
+        return(
+          <div className="card">
+            <img src={user.avatar} key = {user.avatar} className="image" alt="logo512.png" />
+            <h3 className="userName" key ={user.first_name}  >{user.first_name} {user.Last_name}</h3>
+            <p className="mailId" key = {user.email} >{user.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  </div>
+);
+}
+
+export default App;
